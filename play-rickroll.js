1af4a2c75008030e96c4fb493b57bcf928ad99b9
vitouh <donvitoreyes84@gmail.com>
2025-07-08 21:03:02 +0800

    Create play-rickroll.js

---
+++ play-rickroll.js
@@ -0,0 +1,5 @@
+function playRickroll() {
+  document.getElementById(\"playScreen\").innerHTML = `
+    <iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0\" allow=\"autoplay\"></iframe>
+  `;
+}
