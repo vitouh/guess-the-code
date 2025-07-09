37397bb62e72f51013f9de8502782d5cb09ffdd4
vitouh <donvitoreyes84@gmail.com>
2025-07-09 19:05:53 +0800

    Delete play-rickroll.js

--- play-rickroll.js
+++
@@ -1,5 +0,0 @@
-function playRickroll() {
-  document.getElementById(\"playScreen\").innerHTML = `
-    <iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0\" allow=\"autoplay\"></iframe>
-  `;
-}
