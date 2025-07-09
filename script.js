50d72db21a27eed16e681cca645558b8dfce3d7f
vitouh <donvitoreyes84@gmail.com>
2025-07-09 19:36:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,6 @@ function checkCode() {
   }
 }
 
-// === PLAY RICKROLL ===
 function playRickroll() {
   document.getElementById("playScreen").innerHTML = `
     <iframe width="100%" height="100%" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0" allow="autoplay"></iframe>
