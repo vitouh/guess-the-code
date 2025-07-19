1a0a7dcb0dc5e7c3dc67a07f5de2a6333d2359da
vitouh <donvitoreyes84@gmail.com>
2025-07-19 15:54:22 +0800

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,21 +1,20 @@
-const canvas = document.getElementById('matrix');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("matrixCanvas");
+const ctx = canvas.getContext("2d");
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
 const letters = "01";
-const fontSize = 14;
+const fontSize = 16;
 const columns = canvas.width / fontSize;
+const drops = Array.from({ length: columns }).fill(1);
 
-const drops = Array(Math.floor(columns)).fill(1);
-
-function draw() {
+function drawMatrix() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   ctx.fillStyle = "#0F0";
-  ctx.font = fontSize + "px Courier";
+  ctx.font = fontSize + "px monospace";
 
   for (let i = 0; i < drops.length; i++) {
     const text = letters[Math.floor(Math.random() * letters.length)];
@@ -29,4 +28,4 @@ function draw() {
   }
 }
 
-setInterval(draw, 33);
+setInterval(drawMatrix, 33);
