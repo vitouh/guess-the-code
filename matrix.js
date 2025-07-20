2f114c0919cfc88f9817baf4b048f23eadb211f2
vitouh <donvitoreyes84@gmail.com>
2025-07-20 13:00:54 +0800

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -4,27 +4,33 @@ const ctx = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-const chars = "01";
-const fontSize = 14;
-const columns = canvas.width / fontSize;
-const drops = Array(Math.floor(columns)).fill(1);
+let columns = canvas.width / 20;
+let drops = Array(Math.floor(columns)).fill(1);
 
-function draw() {
+function drawMatrix() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#0f0";
-  ctx.font = fontSize + "px monospace";
+
+  ctx.fillStyle = "#0F0";
+  ctx.font = "20px monospace";
 
   for (let i = 0; i < drops.length; i++) {
-    const char = chars.charAt(Math.floor(Math.random() * chars.length));
-    ctx.fillText(char, i * fontSize, drops[i] * fontSize);
-    if (drops[i] * fontSize > canvas.height || Math.random() > 0.975) {
+    const text = Math.random() > 0.5 ? "0" : "1";
+    ctx.fillText(text, i * 20, drops[i] * 20);
+
+    if (drops[i] * 20 > canvas.height && Math.random() > 0.975) {
       drops[i] = 0;
     }
+
     drops[i]++;
   }
 }
 
-setInterval(draw, 50);
+setInterval(drawMatrix, 50);
 
-window.addEventListener("resize", () => location.reload());
+window.addEventListener('resize', () => {
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  columns = canvas.width / 20;
+  drops = Array(Math.floor(columns)).fill(1);
+});
